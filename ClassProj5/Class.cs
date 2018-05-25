8dad50822b8bd130c40b6a817e9972069ff0c92b
kim-young <younghyun87@gmail.com>
2018-05-25 15:41:08 -0400

    Project on page 41

---
+++ ClassProj5/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ClassProj5
+{
+    public class Class
+    {
+        public Class()
+        {
+        }
+
+        public static void giveInt(int numberOne)
+		{
+			int result = numberOne / 2;         
+		}
+    }
+}
---
+++ ClassProj5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassProj5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Input number.");
+			Class.giveInt(int.Parse(Console.ReadLine()));
+
+			Console.WriteLine();
+        }
+    }
+}
